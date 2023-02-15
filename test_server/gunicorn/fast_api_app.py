0ee86635e8f85297afccfbba41d0c824c649eb16
nikkitkit007 <nikkitkit0707@mail.ru>
2023-02-16 00:19:27 +0300

    add tests

---
+++ test_server/gunicorn/fast_api_app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
